fe379c8e40043d8ff35864fe96f6b8cb1d8f979f
andre327 <andrefigueroa005@gmail.com>
2022-02-23 16:30:45 -0500

    Subiendo carpetas

---
+++ js/manipular_imagenes.js
@@ -0,0 +1,41 @@
+//colocamos el cursor y la imagen nos envian una alert
+function principal(){
+ 
+}
+//colocar la imagen invisible
+//function invisible(){
+  //  document.getElementById("segunda").className = "img-invisible";
+//}
+
+
+
+function cambiarImagen(id,ruta){
+    let imgnueva = document.getElementById(id);
+ imgnueva.src= ruta; 
+  
+}
+
+// function segunda(){
+//     document.getElementById("primera").src = "img/740x500.jpg";
+// }
+// function segunda2(){
+//     document.getElementById("primera").src = "img/740x500dos.jpg";
+// }
+// function segunda3(){
+//     document.getElementById("primera").src = "img/740x500.jpg";
+// }
+
+
+
+
+
+
+
+
+//cambiamos la imagen por otra y ademas cuando quitamos el cursor se devuelve a la imagen original
+//function cambiarImagen(){
+  //  document.getElementById("siguiente").src = "img/3.jpg";
+//}
+//function retornarImagen(){
+  //  document.getElementById("siguiente").src= "img/2.jpg";
+//}
